52c67c03c3c522769d1fae1156135bf7ebb6c042
kepatotorica <kepatoto@gmail.com>
2018-12-10 19:50:05 -0700

    more changes to accomidate this project

--- src/main/java/edu/boisestate/cs410/charity/GradeBook.java
+++ src/main/java/edu/boisestate/cs410/charity/GradeBook.java
@@ -26,14 +26,23 @@ public class GradeBook {
 // not:  new-class Nathaniel Spring 2007 1 desc
     @Command
     public void newClass(String pName, String pTerm, int pYear, int pSection, String pDescription) throws SQLException {
+
+        int c_id = classCreateOrId(pName, pTerm, pYear, pSection, pDescription);
+
+    }
+
+    @Command
+    public int classCreateOrId(String pName, String pTerm, int pYear, int pSection, String pDescription) throws SQLException {
         System.out.println("adding a new class");
         Boolean any = false;
+        int c_id = -1;
         String queryCheck =
                 "SELECT * from class where name='" + pName + "' and term='" + pTerm + "' and year='" + pYear + "'";
         try (PreparedStatement stmt = db.prepareStatement(queryCheck)) {
             try (ResultSet rs = stmt.executeQuery()) {
                 while (rs.next()) {
                     any = true;
+                    c_id = rs.getInt("c_id");
                 }
                 System.out.println("any matches? " + any );
             }
@@ -46,11 +55,42 @@ public class GradeBook {
                             "values ('" + pName + "', '" + pTerm + "', " + pYear + ", '" + pDescription + "')";
 
             try (PreparedStatement stmt = db.prepareStatement(query)) {
-//                try {
-                    stmt.executeUpdate();
-//                }catch(){
+                stmt.executeUpdate();
+            }
 
-//                }
+            try (PreparedStatement stmt = db.prepareStatement(queryCheck)) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while (rs.next()) {
+                        c_id = rs.getInt("c_id");
+                    }
+                    System.out.println("any matches? " + any );
+                }
+            }
+        }
+        return c_id;
+    }
+
+    @Command
+    public void newSection(String class, ) throws SQLException {
+        System.out.println("searching for author matching " + donorName);
+        String query =
+                "SELECT donor_id, donor_name, sum(amount) AS total " +
+                        "FROM gift JOIN gift_fund_allocation USING(gift_id) " +
+                        "JOIN donor USING(donor_id) " +
+                        "WHERE donor_name ILIKE ('%' ||'" + donorName  + "'|| '%') " +
+                        "GROUP BY donor_id, donor_name";
+        try (PreparedStatement stmt = db.prepareStatement(query)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                System.out.format("Donors matching %s:%n", donorName);
+                System.out.println("\t|------------------------------");
+                System.out.println("\t|Donor Name | Donor Id | Total");
+                System.out.println("\t|------------------------------");
+                while (rs.next()) {
+                    int aid = rs.getInt("donor_id");
+                    String name = rs.getString("donor_name");
+                    int amount = rs.getInt("total");
+                    System.out.format("\t|%s | %d | %d%n", name, aid, amount);
+                }
             }
         }
     }
