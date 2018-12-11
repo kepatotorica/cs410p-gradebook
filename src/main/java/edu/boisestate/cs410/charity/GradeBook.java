bb87dd5c973af71447c9bba0754060ef7c288c5d
kepatotorica <kepatoto@gmail.com>
2018-12-10 18:53:51 -0700

    made some changes to the database

--- src/main/java/edu/boisestate/cs410/charity/GradeBook.java
+++ src/main/java/edu/boisestate/cs410/charity/GradeBook.java
@@ -22,6 +22,31 @@ public class GradeBook {
         }
     }
 
+    @Command
+    public void newClass(String donorName) throws SQLException {
+        System.out.println("searching for author matching " + donorName);
+        String query =
+                "SELECT donor_id, donor_name, sum(amount) AS total " +
+                        "FROM gift JOIN gift_fund_allocation USING(gift_id) " +
+                        "JOIN donor USING(donor_id) " +
+                        "WHERE donor_name ILIKE ('%' ||'" + donorName  + "'|| '%') " +
+                        "GROUP BY donor_id, donor_name";
+        try (PreparedStatement stmt = db.prepareStatement(query)) {
+            try (ResultSet rs = stmt.executeQuery()) {
+                System.out.format("Donors matching %s:%n", donorName);
+                System.out.println("\t|------------------------------");
+                System.out.println("\t|Donor Name | Donor Id | Total");
+                System.out.println("\t|------------------------------");
+                while (rs.next()) {
+                    int aid = rs.getInt("donor_id");
+                    String name = rs.getString("donor_name");
+                    int amount = rs.getInt("total");
+                    System.out.format("\t|%s | %d | %d%n", name, aid, amount);
+                }
+            }
+        }
+    }
+
 //    @Command
 //    public void findDonor(String donorName) throws SQLException {
 //        System.out.println("searching for author matching " + donorName);
@@ -46,7 +71,7 @@ public class GradeBook {
 //            }
 //        }
 //    }
-// 
+//
 //    @Command
 //    public void donorReport(String donorId, String year) throws SQLException {
 //        totalGifts(donorId, year);
