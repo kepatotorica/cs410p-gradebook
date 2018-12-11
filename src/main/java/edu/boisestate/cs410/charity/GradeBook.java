6c40c2800cd9d72c3ef4322325567fc4bf8df915
kepatotorica <kepatoto@gmail.com>
2018-12-10 19:34:03 -0700

    almost finished the first query

--- src/main/java/edu/boisestate/cs410/charity/GradeBook.java
+++ src/main/java/edu/boisestate/cs410/charity/GradeBook.java
@@ -22,27 +22,35 @@ public class GradeBook {
         }
     }
 
+// in:   new-class Nathaniel Spring 2006 1 desc
+// not:  new-class Nathaniel Spring 2007 1 desc
     @Command
-    public void newClass(String donorName) throws SQLException {
-        System.out.println("searching for author matching " + donorName);
-        String query =
-                "SELECT donor_id, donor_name, sum(amount) AS total " +
-                        "FROM gift JOIN gift_fund_allocation USING(gift_id) " +
-                        "JOIN donor USING(donor_id) " +
-                        "WHERE donor_name ILIKE ('%' ||'" + donorName  + "'|| '%') " +
-                        "GROUP BY donor_id, donor_name";
-        try (PreparedStatement stmt = db.prepareStatement(query)) {
+    public void newClass(String pName, String pTerm, int pYear, int pSection, String pDescription) throws SQLException {
+        System.out.println("adding a new class");
+        Boolean any = false;
+        String queryCheck =
+                "SELECT * from class where name='" + pName + "' and term='" + pTerm + "' and year='" + pYear + "'";
+        try (PreparedStatement stmt = db.prepareStatement(queryCheck)) {
             try (ResultSet rs = stmt.executeQuery()) {
-                System.out.format("Donors matching %s:%n", donorName);
-                System.out.println("\t|------------------------------");
-                System.out.println("\t|Donor Name | Donor Id | Total");
-                System.out.println("\t|------------------------------");
                 while (rs.next()) {
-                    int aid = rs.getInt("donor_id");
-                    String name = rs.getString("donor_name");
-                    int amount = rs.getInt("total");
-                    System.out.format("\t|%s | %d | %d%n", name, aid, amount);
+                    any = true;
                 }
+                System.out.println("any matches? " + any );
+            }
+        }
+
+        if(!any) {
+            System.out.println("in the add");
+            String query =
+                    "insert into class (name, term, year, description) \n" +
+                            "values ('" + pName + "', '" + pTerm + "', " + pYear + ", '" + pDescription + "')";
+
+            try (PreparedStatement stmt = db.prepareStatement(query)) {
+//                try {
+                    stmt.executeUpdate();
+//                }catch(){
+
+//                }
             }
         }
     }
